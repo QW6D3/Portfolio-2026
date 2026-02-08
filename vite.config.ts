bc44f6eaf03b089eac5a855813256d914d10093f
QW6D3 <149207814+QW6D3@users.noreply.github.com>
2026-02-08 23:55:25 +0100

    chore: stabilize CI/CD pipeline (fix vitest config & prettier formatting)

--- vite.config.ts
+++ vite.config.ts
@@ -2,14 +2,14 @@ import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vitest/config'; // On change l'import ici
 
 export default defineConfig({
-    plugins: [sveltekit()],
-    test: {
-        // Cette ligne dit à Vitest de chercher n'importe quel fichier .ts 
-        // dans ton dossier src/test/, même s'il n'a pas ".test" dans le nom
-        include: ['src/test/*.ts', 'src/**/*.{test,spec}.ts'],
-        
-        // Optionnel : permet d'utiliser 'describe', 'it', 'expect' sans les importer
-        globals: true, 
-        environment: 'jsdom',
-    }
+	plugins: [sveltekit()],
+	test: {
+		// Cette ligne dit à Vitest de chercher n'importe quel fichier .ts
+		// dans ton dossier src/test/, même s'il n'a pas ".test" dans le nom
+		include: ['src/test/*.ts', 'src/**/*.{test,spec}.ts'],
+
+		// Optionnel : permet d'utiliser 'describe', 'it', 'expect' sans les importer
+		globals: true,
+		environment: 'jsdom'
+	}
 });
